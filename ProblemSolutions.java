8e13cf3a7343546e15837a26398b66ff3ef2922b
BlakemaiD <150864910+BlakemaiD@users.noreply.github.com>
2024-11-21 22:19:19 -0600

    Added name & section
    Just added my name and section!

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,6 +1,6 @@
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *   Blake / 002
  *
  *   This java file contains the problem solutions for the methods selectionSort,
  *   mergeSortDivisibleByKFirst, asteroidsDestroyed, and numRescueCanoes methods.
